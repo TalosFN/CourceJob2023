ee9e410d84a572a28a12fb9cc626604a9609b235
TalosFN <kirill.star.1408@gmail.com>
2023-05-11 22:37:19 +0300

    First Commit

---
+++ src/main/java/entity/Cell.java
@@ -0,0 +1,10 @@
+package entity;
+
+public class Cell {
+
+	private int id;
+	
+	private int size;
+	
+	
+}
---
+++ src/main/java/entity/Prisoner.java
@@ -0,0 +1,12 @@
+package entity;
+
+public class Prisoner {
+	
+	private int id;
+
+	private String name;
+	
+	private String crime;
+	
+	
+}
